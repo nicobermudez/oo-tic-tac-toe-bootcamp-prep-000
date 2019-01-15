2d372650c9e5e67657f13f6e2f0696b4836b063b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-15 14:56:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,6 +90,16 @@ def winner
 end
 end
 
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
   
 
