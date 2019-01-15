873cd590381a2ff809671a3d290186f0b4430f91
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-15 14:37:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,10 +16,13 @@ WIN_COMBINATIONS = [
   [6, 4, 2]
 ]
 
-def display_board(board)
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts '-----------'
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts '-----------'
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+en
+
+def input_to_index(input)
+  input.to
