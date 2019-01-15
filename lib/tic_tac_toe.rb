4477de8821a9adaccb31562c3e79020fcfcacaf2
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-15 14:57:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,17 +77,10 @@ def over?
   (@board.won? || @board.draw)
 end
 
-def winner
-  WIN_COMBINATIONS.detect do |win_combo|
-    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-      return "X"
-    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-      return "O"
-    else
-      nil
-    end
+def winner(board)
+  if winning_combo = @board.won?
+    board[winning_combo.first]
   end
-end
 end
 
 def play
